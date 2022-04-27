2ea6f44eae4903d1033f17101a2e8b93583404ec
miunmnn <esteban.principe@utec.edu.pe>
2022-04-27 11:08:55 -0500

    Update created post url

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.get('/get-users', getUsers);
 
+router.post(
+  "/create-user",  
+  body('first_name').isLength({ min: 1 }).trim().withMessage('First Name min length is 1'),
+  body('last_name').isLength({ min: 1 }).trim().withMessage('Last Name min length is 1'),
+  body('email').isEmail().withMessage('Email is invalid'),
+  body('note').isLength({ min: 1 }).trim().withMessage('Note min length is 1')
+);
+
 export default router;
