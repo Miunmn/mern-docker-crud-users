e1e6ed3e11ccaf5b0e463499f90c288bb1c9031e
miunmnn <esteban.principe@utec.edu.pe>
2022-04-27 11:09:49 -0500

    Update added all the urls and methods

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,4 +1,5 @@
-import { getUsers } from '../controllers/usersController.js';
+import { getUsers, createUser, updateUser, deleteUser } from '../controllers/usersController.js';
+import { body, query } from 'express-validator';
 
 import express from 'express';
 
@@ -12,6 +13,20 @@ router.post(
   body('last_name').isLength({ min: 1 }).trim().withMessage('Last Name min length is 1'),
   body('email').isEmail().withMessage('Email is invalid'),
   body('note').isLength({ min: 1 }).trim().withMessage('Note min length is 1')
+);
+
+router.put("/update-user", 
+  query('id').isLength({ min: 1 }).trim().withMessage('Specify id'),
+  body('first_name').optional().isLength({ min: 1 }).trim().withMessage('First Name min length is 1 '),
+  body('last_name').optional().isLength({ min: 1 }).trim().withMessage('Last Name min length is 1'),
+  body('email').optional().isEmail().withMessage('Email is invalid'),
+  body('note').optional().isLength({ min: 1 }).trim().withMessage('Note min length is 1'),
+  updateUser
+);
+
+router.delete("/delete-user", 
+  query('id').isLength({ min: 1 }).trim().withMessage('Specify id'),
+  deleteUser
 );
 
 export default router;
